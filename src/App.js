e1c2f918f08dbed4dd6c6533dffa1d553d88089a
Saeed Jabbar <saeedryanjabbar@gmail.com>
2021-11-08 12:49:24 -0500

    base styling added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import TodoItem from "./components/TodoItem"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container card mt-5 pt-3">
+      <div className="todo-list">
+        <TodoItem />
+        <TodoItem />
+        <TodoItem />
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/TodoItem.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+const TodoItem = () => {
+  return (
+    <div className="todo-item mb-3">
+      <div className="form-check bg-info px-5 py-3">
+        <input className="form-check-input" type="checkbox" value=""/>
+        <label className="form-check-label">Placeholder text here</label>
+      </div>
+    </div>
+  )
+}
+
+export default TodoItem;
